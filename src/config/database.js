926eb3a3e8f125bdfdcc4bbb4ef0c36f0fa3793c
Ferhatyusuf92 <ferhatyusuf92@gmail.com>
2024-09-27 18:38:29 +0200

    repo was updated

--- src/config/database.js
+++ src/config/database.js
@@ -1,17 +1,42 @@
 const mysql = require("mysql2/promise");
+const dotenv = require("dotenv");
+dotenv.config({ path: "./.env" });
 
 let connection;
+
+const shouldResetTables = true;
+
 const initializeConnection = async () => {
   try {
     connection = await mysql.createConnection({
-      host: "localhost",
-      user: "root",
-      password: "Ff123456",
-      port: 3306,
-      database: "backend_app",
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      port: process.env.DB_PORT,
+      database: process.env.DB_DATABASE,
     });
+    if (shouldResetTables) {
+      await resetTable();
+    }
 
     console.log("Database connected");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const resetTable = async () => {
+  const dropUserTableQuery = ` DROP TABLE IF EXISTS USERS`;
+  const createUserTableQuery = `CREATE TABLE USERS (
+  id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  username varchar(255) not null,
+  email varchar(255) not null,  
+  password varchar(200) not null);`;
+
+  try {
+    await connection.query(dropUserTableQuery);
+    await connection.query(createUserTableQuery);
+    console.log("Table reset");
   } catch (error) {
     console.log(error);
   }
