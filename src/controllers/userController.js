4eee96bfc581a7029bb4ca4cd74b6d7f71815166
Ferhatyusuf92 <ferhatyusuf92@gmail.com>
2024-10-04 20:15:27 +0200

    App was updated

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,42 +1,72 @@
-const userRepository = require('../repostories/userRepository.js');
+const { status } = require("express/lib/response.js");
+const userRepository = require("../repostories/userRepository.js");
 
-
-const getUsers = async(req, res) => {
-    try {
-      const users = await userRepository.getUsers();
-      res.status(200).send(users);      
-    } catch (error ) {
-      res.status(500).send({ message: " Database error"});
-    }
+const getUsers = async (req, res) => {
+  try {
+    const users = await userRepository.getUsers();
+    res.status(200).send(users);
+  } catch (error) {
+    res.status(500).send({ message: " Database error" });
+  }
 };
 
-const getUserById = (req, res) => {
-    res.send(userRepository.getUserById());
+const getUserById = async (req, res) => {
+  try {
+    const result = await userRepository.getUserById(Number(req.params.id));
+    if (result) {
+      res.status(200).send(result);
+    } else {
+      res.status(404).send({ message: "User not found!" });
+    }
+  } catch (error) {
+    res.status(500).send({ message: "Database error" });
+  }
 };
 
 const createUser = async (req, res) => {
   console.log(req.body);
-    try {
-      const user = await userRepository.createUser(req.body);
-      res.status(201).send(user);
-    } catch (error) {
-      res.status(500).send({ message : "Database error" });
-    }
-  };
-
-const updateUser = (req, res) => {
-    res.send(userRepository.updateUser());
+  try {
+    const user = await userRepository.createUser(req.body);
+    res.status(201).send(user);
+  } catch (error) {
+    res.status(500).send({ message: "Database error" });
+  }
 };
 
-const deleteUser = (req, res) => {
-    res.send(userRepository.deleteUser());
+const updateUser = async (req, res) => {
+  try {
+    const result = await userRepository.updateUser(
+      Number(req.params.id),
+      req.body
+    );
+    if (result) {
+      res.status(200).send(result);
+    } else {
+      res.status(404).send({ message: "User not found!" });
+    }
+  } catch (error) {
+    res.status(500).send({ message: "Database error" });
+  }
+};
+
+const deleteUser = async (req, res) => {
+  try {
+    const result = await userRepository.deleteUser(Number(req.params.id));
+
+    if (result) {
+      res.status(200).send(result);
+    } else {
+      res.status(404).send({ message: "User not found!" });
+    }
+  } catch (error) {
+    res.status(500).send({ message: "Database error" });
+  }
 };
 
 module.exports = {
-    getUsers,
-    getUserById,
-    createUser,
-    updateUser,
-    deleteUser
+  getUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
 };
-
--- src/repostories/userRepository.js
+++ src/repostories/userRepository.js
@@ -1,36 +1,63 @@
-const { getConnection } = require('../config/database.js');
+const { getConnection } = require("../config/database.js");
 
-const getUsers =  async() => {    
-    const connection = getConnection();
-    const sql = 'SELECT * FROM USERS';
-    const [users] = await connection.query(sql);
-    return users;
+const getUsers = async () => {
+  const connection = getConnection();
+  const sql = "SELECT * FROM USERS";
+  const [users] = await connection.query(sql);
+  return users;
 };
 
-const getUserById = (req, res) => {   
-    
+const getUserById = async (id) => {
+  const connection = getConnection();
+  const sql = "SELECT * FROM users WHERE id = ?";
+  const [result] = await connection.query(sql, [id]);
+  if (result.lenght === 0) {
+    return "";
+  }
+  return result[0];
 };
-
 
 const createUser = async (body) => {
-    const connection = getConnection();
-    const sql = 'INSERT INTO users (username, email , password) VALUES (?, ?,?)';
-    const [result] = await connection.query(sql, [body.username, body.email, body.password]);
-    return { id: result.insertId, ...body };
-  };
-
-const updateUser = (req, res) => {
-   return 'Update user'
+  const connection = getConnection();
+  const sql = "INSERT INTO users (username, email , password) VALUES (?, ?,?)";
+  const [result] = await connection.query(sql, [
+    body.username,
+    body.email,
+    body.password,
+  ]);
+  return { id: result.insertId, ...body };
 };
 
-const deleteUser = (req, res) => {
-    return 'Delete user'
+const updateUser = async (id, body) => {
+  const connection = getConnection();
+  const sql =
+    "UPDATE users SET username = ?, email = ? , password = ? WHERE id = ?";
+  const [result] = await connection.query(sql, [
+    body.username,
+    body.email,
+    body.password,
+    id,
+  ]);
+  if (result.affectedRows === 0) {
+    return "";
+  }
+  return { id: id, ...body };
+};
+
+const deleteUser = async (id) => {
+  const connection = getConnection();
+  const sql = "DELETE FROM users WHERE id = ?";
+  const [result] = await connection.query(sql, [id]);
+  if (result.affectedRows === 0) {
+    return "";
+  }
+  return "User deleted";
 };
 
 module.exports = {
-    getUsers,
-    getUserById,
-    createUser,
-    updateUser,
-    deleteUser
+  getUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
 };
