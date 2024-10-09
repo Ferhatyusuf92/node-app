c959c200fa89ce96d3807377636a77f4d66cc2e9
Ferhatyusuf92 <ferhatyusuf92@gmail.com>
2024-10-09 18:17:34 +0200

    App updated with sequelize

--- src/index.js
+++ src/index.js
@@ -1,21 +1,20 @@
 const express = require("express");
 const users = require("./routes/userRoute.js");
-const { initializeConnection } = require("./config/database.js");
-const bodyParser = require('body-parser')
-
-
+const sequelize = require("./config/database.js");
+const bodyParser = require("body-parser");
 
 const app = express();
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 const port = 3000;
 
 app.use("/users", users);
 
 app.listen(port, async () => {
-  await initializeConnection();
+  await sequelize.authenticate();
+  console.log("Connection has been established successfully.");
   console.log(`Example app listening on port ${port}`);
 });
